a7eb8d0cc1910642424d1f1f446719aec2ebac85
Miguel <miguelpellicergrau@gmail.com>
2020-01-17 21:01:03 +0100

    second commit

--- REST-API/controllers/noticiaController.js
+++ REST-API/controllers/noticiaController.js
@@ -17,12 +17,37 @@ async function getAllNoticias(req, res) {
     res.status(200).send(noticias);
 }
 
+/**
+ * Metodo que te devuelve una noticia dado el ID
+ * @param req
+ * @param res
+ */
 async function getNoticia(req, res){
+    const noticia = await Noticia.findOne({_id: req.user});
+    if (!noticia)
+        return res.status(404).send('No se ha encontrado la noticia');
 
+    res.status(200).send({
+        "titulo": noticia.titulo,
+        "resumen": noticia.resumen,
+        "cuerpo": noticia.cuerpo,
+        "imagen": noticia.imagen
+    });
 }
 
+/**
+ * Metodo que añade una Noticia
+ * @param req
+ * @param res
+ */
 async function addNoticia(req, res){
-
+    const noticia = new Noticia({
+        titulo: res.body.titulo,
+        resumen: res.body.resumen,
+        cuerpo: res.body.cuerpo,
+        imagen: res.body.imagen
+    });
+    //TODO terminar esto y guardar la imagen
 }
 
 async function updateNoticia(req, res){
