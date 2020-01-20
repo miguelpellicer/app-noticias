542ee1d95bdd96f806f38f43c301c144bf43e181
Miguel <miguelpellicergrau@gmail.com>
2020-01-20 17:39:18 +0100

    Added navigation on admin site

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NoticiaListaComponent} from "./components/noticia-lista/noticia-lista.component";
+import {NoticiaFormComponent} from "./components/noticia-form/noticia-form.component";
 
 
-const routes: Routes = [];
+// SE CONFIGURAN LAS RUTAS
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/noticias',
+    pathMatch: 'full'
+  },
+  {
+    path: 'noticias',
+    component: NoticiaListaComponent
+  },
+  {
+    path: 'noticias/add',
+    component: NoticiaFormComponent
+  },
+  {
+    path: 'noticias/edit/:id',
+    component: NoticiaFormComponent
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { NoticiaListaComponent } from './components/noticia-lista/noticia-lista.component';
+import { NoticiaFormComponent } from './components/noticia-form/noticia-form.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NoticiaListaComponent,
+    NoticiaFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
